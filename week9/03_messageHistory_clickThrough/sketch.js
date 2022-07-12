8e2e8efc0412c6fdc0de0b54500b1e3ba09c97c6
smyuri <105171669+smyuri@users.noreply.github.com>
2022-07-12 15:59:04 -0400

    Add files via upload

---
+++ week9/03_messageHistory_clickThrough/sketch.js
@@ -0,0 +1,132 @@
+
+
+let dataServer;
+let pubKey = "pub-c-6f13f86f-1373-45d0-8d30-ebbea8f73496";
+let subKey = "sub-c-5c7c93ad-42b7-4af3-9cce-e78ea25ba5c2";
+let secretKey = "sec-c-ZmZjMTE2NWEtNzE3OS00ZWUwLWFiOGMtN2NlNDlkZDRkYmIy";
+
+let channelName = "history";
+
+let you;
+
+//input variables for the form to PubNub
+
+let count = 0;
+let historyLength;
+let allMessages = [];
+
+function preload() { 
+
+  // logic to create a random UUID
+    you = random(0,1000000); 
+    console.log(you);
+    you = int(you);
+    console.log(you);
+    you = you.toString();
+  
+}
+
+
+function setup() {
+
+    createCanvas(windowWidth, windowHeight);
+
+    dataServer = new PubNub({
+      subscribeKey: subKey,
+      publishKey: pubKey,
+      uuid: you,
+      secretKey: secretKey,
+      heartbeatInterval: 0,
+    });
+
+     // listen for messages coming through the subcription feed on this specific channel. 
+
+    dataServer.subscribe({ channels: [channelName] });
+    dataServer.addListener({ message: readIncoming });
+   
+  
+    textAlign(CENTER);
+  
+
+    fetchMessages();
+
+}
+  
+function draw() {
+ 
+  background(255);
+  textSize(40);
+  text(allMessages[count], windowWidth/2, windowHeight/2);
+
+}
+
+function mousePressed() {
+
+  if (count < historyLength) {
+    count++;
+
+  } else {
+    count = 0;
+  }
+
+
+}
+
+
+function fetchMessages() {
+console.log("fetching");
+
+  dataServer.fetchMessages(
+    {
+        channels: [channelName],
+        end: '15343325004275466',
+        count: 100
+    },
+    (status, response) => {
+     console.log(response.channels.history);
+      
+      historyLength = response.channels.history.length;
+
+      for (let i = 0; i < historyLength; i++) {
+    
+        append(allMessages,response.channels.history[i].message.messageText);
+
+    }
+    }
+  );
+   
+}
+
+function drawMessages(messageHistory) {
+
+  console.log("in draw messages");
+
+  console.log(messageHistory);
+  textSize(80);
+  for (let i = 0; i < messageHistory.length; i++) {
+    
+      console.log(messageHistory[i]);
+      text(messageHistory[i].message.messageText, windowWidth/2, 100 * (i+1));
+
+  }
+
+}
+  // PubNub logic below
+function sendTheMessage() {
+  // Send Data to the server to draw it in all other canvases
+  dataServer.publish({
+    channel: channelName,
+    message: {
+      messageText: sendText.value()
+    },
+  });
+
+  sendText.value("");
+
+}
+
+function readIncoming(inMessage) {
+  console.log(inMessage);
+  fetchMessages();
+
+}
